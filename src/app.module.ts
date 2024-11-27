d6bdd05315f4f9cc4704fdfa0181f3382ec35700
Jin-hyeok2 <hjhlove1120@gmail.com>
2024-11-27 22:10:52 +0900

    rest commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberController } from './member/member.controller';
+import { MemberService } from './member/member.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '1q2w3e4r',
+        database: 'fornest',
+        synchronize: true,
+        entities: []
+      })
+  ],
+  controllers: [AppController, MemberController],
+  providers: [AppService, MemberService],
 })
 export class AppModule {}
